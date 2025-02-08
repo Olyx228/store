a34cd83a79cf5f68b21d095528b1c9b5003b07df
Семён Нефедов <semennefiodov@yandex.ru>
2025-02-08 20:14:15 +0700

    fix Bag

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,6 +1,14 @@
-﻿namespace Store;
+﻿using System;
+namespace Store;
 
-public class Class1
+public class Book
 {
+    public int Id { get; }
+    public string Title { get; }
+    public Book(int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
 
 }
--- infastructure/Store.Memory/BookRepository.cs
+++ infastructure/Store.Memory/BookRepository.cs
@@ -1,7 +1,19 @@
-﻿namespace Store.Memory
-{
-    public class Class1
-    {
+﻿using System;
+using System.Linq; 
 
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+{
+                new Book(1, "Art of Programing"),
+                new Book(2, "Refactoring"),
+                new Book(3, "C Programing Language"),
+            };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
     }
 }
