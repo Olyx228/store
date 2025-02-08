11d320e8680a698437d97d19c10f0edd56d5d623
Семён Нефедов <semennefiodov@yandex.ru>
2025-02-08 22:47:41 +0700

    Ne daet books

--- presentation/store.Web/Program.cs
+++ presentation/store.Web/Program.cs
@@ -1,15 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Store;
+using Store.Memory;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+ 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IBookRepository, BookRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
