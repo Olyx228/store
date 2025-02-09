1ce71ea30d23d0b34eac2fb3e15e48b6494b1672
Семён Нефедов <semennefiodov@yandex.ru>
2025-02-09 18:28:12 +0700

    SearchInAuthor and Title and Ibns

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -4,10 +4,14 @@ namespace Store;
 public class Book
 {
     public int Id { get; }
+    public string Author { get; }
+    public string Isbn { get; }
     public string Title { get; }
-    public Book(int id, string title)
+    public Book(int id, string isbn, string author, string title)
     {
         Id = id;
+        Isbn = isbn;
+        Author = author;
         Title = title;
     }
 
